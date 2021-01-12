38b72535cc04bdfbb18fe30f3ddcf62c582180ae
Kennedy Wambua <kennywambu254@gmail.com>
2021-01-12 19:52:44 +0300

    adding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
