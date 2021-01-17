27ff0dd99825a3a609e17196a1e556ba0b1a0f73
Kennedy Wambua <kennywambu254@gmail.com>
2021-01-17 21:19:03 +0300

    add all

--- Program.cs
+++ Program.cs
@@ -6,7 +6,107 @@ namespace Gaming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] array = new int[10];//best way of creating an array. This is referennce type
+            int[] ken = { };// inline imitialisation. This is value type
+            try
+            {
+                string laser ;
+                Console.WriteLine("killer strike or hit armed, Shoot your target: ");
+                laser = Console.ReadLine();
+                laser = laser.ToUpper();
+                //  Spaceship s = new Spaceship();
+                Spaceship c = new combatship();
+                Spaceship k = new cargoship();
+                cargoship cargo = new cargoship();
+                combatship combat = new combatship();
+                //  foo.ProcessSpaceship(cargo);
+                // foo.ProcessSpaceship(combat);
+                Spaceship[] s = new Spaceship[4];
+                s[0] = new combatship();
+                s[1] = new cargoship();
+                s[2] = new cargoship();
+                s[3] = new combatship();
+                for(int i  = 0; i < 4; i++)
+                {
+                    s[i].LaserHit();
+                }
+
+                //creating a multi-dimensional  array
+                int[,] array2d;
+                int[,,] array3d;
+                array2d = new int[5, 5];
+                array3d = new int[5, 5, 3];
+
+                //accessing the arrays
+                array2d[0, 0] = 100; //the top left box
+                array3d[2, 2, 1] = 400; // the middle cube
+
+                //creating a multi-dimensional array in the java way.
+
+                int[][] arry2d;
+                int[][][] arry3d;
+                // intialising the arrays in ths way is a bit trciky
+
+                arry2d = new int[5][];
+                for (int j = 0; j < 5; j++)
+                {
+                    arry2d[j] = new int[5];
+                }
+                arry2d[0][0] = 100;
+                Console.WriteLine(arry2d);
+
+                if (laser == "HIT CARGO")
+                {
+                    k.LaserHit();
+                }else if (laser == "HIT")
+                {
+                    c.LaserHit();
+                }
+                else
+                {
+                    Console.WriteLine("No function has be called to fire up");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("you've entered the wrong command please check the type");
+            }
+        }
+    }
+   public class Spaceship
+    {
+        public virtual
+            void LaserHit()
+        {
+            Console.WriteLine("Hit and run");
+        }
+
+    }
+    class combatship : Spaceship
+    {
+        public override void LaserHit()
+        {
+            Console.WriteLine("Less Damage!!!");
+            //base.LaserHit();
+            //less damage
+        }
+    }
+    class cargoship : Spaceship
+    {
+        public override void LaserHit()
+        {
+           // throw new NotImplementedException();
+            Console.WriteLine("Cargo damged");
+        }
+    }
+    class foo
+    {
+        static void ProcessSpaceship( Spaceship s)
+        {
+            s.LaserHit();
+            Console.WriteLine("destroy all space craft");
         }
     }
 }
